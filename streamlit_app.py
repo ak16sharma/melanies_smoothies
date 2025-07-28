020e554555eb797b787cc173c8e0c9b754355fec
ak16sharma <ak16sharma@gmail.com>
2025-07-28 12:26:58 +0530

    Update streamlit_app.py
    added for

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,11 +50,22 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+name_on_order+ """!!""" , icon="✅")
 
 
-#New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
+
+if ingdredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+      ingredients_string += fruit_chosen+''
+      smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
+  sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
+
+
+
+
+
+#New section to display smoothiefroot nutrition information
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
 
     #if ingredients_string:
         #session.sql(my_insert_stmt).collect()
