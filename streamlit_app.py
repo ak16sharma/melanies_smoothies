642883f901a20969abc2d75ee6f0c45245e2c563
ak16sharma <ak16sharma@gmail.com>
2025-07-28 12:24:22 +0530

    Update streamlit_app.py
    dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,8 +53,8 @@ if ingredients_list:
 #New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
-st.text(smoothiefroot_response.json())
-
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width = True)
 
     #if ingredients_string:
         #session.sql(my_insert_stmt).collect()
