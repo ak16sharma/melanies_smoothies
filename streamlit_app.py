4ecd342cbe4a22cae8a3305f0e7e485e034cf683
ak16sharma <ak16sharma@gmail.com>
2025-07-28 12:22:59 +0530

    Update streamlit_app.py
    json added

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ if ingredients_list:
 #New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
     #if ingredients_string:
